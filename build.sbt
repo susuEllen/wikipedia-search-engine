859c40af324132e658509e002da1ae3f52c40851
Ellen Wong <ellen.l.wong@gmail.com>
2016-11-20 22:49:42 -0800

    checking in start of Wikipedia search relevance project

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "WikipediaSearchProject"
+
+version := "1.0"
+
+//scalaVersion := "2.12.0"
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.2",
+  "org.apache.spark" %% "spark-mllib" % "2.0.2"
+)
---
+++ src/main/scala/WikipediaSearchEngine.scala
@@ -0,0 +1,16 @@
+/**
+ * Created by ellenwong on 11/20/16.
+ */
+object WikipediaSearchEngine {
+  def main(args: Array[String]): Unit = {
+    println("Hello! Lets build a wikipedia search engine")
+  }
+}
+
+class CorpusSearchEngine {
+  // state stored in here
+
+  def run() = {
+    // all the logic can be in here
+  }
+}
